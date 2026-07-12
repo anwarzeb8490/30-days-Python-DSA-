c533096518c1ae49b35a6ecd347c4afc8e35292e
anwarzeb8490 <anwarzeb8490@gmail.com>
2026-07-12 12:28:59 +0500

    program-comparison operator

---
+++ python2026/4-comparison-pp.py
@@ -0,0 +1,22 @@
+# return true false
+
+
+print(5==5)
+print(5!=3)
+print(7>3)
+print(2<1)
+print(5>=5)
+print(4>=3)
+
+#logical operator combine multiple condition together
+# and     both are true
+# or      At least on true
+# not     opposite
+
+print("Logical operator")
+chemistry =45
+physics= 34
+
+print(chemistry>33 and physics>33)
+print(chemistry>50 or physics>33)
+print(not chemistry>33)
---
+++ python2026/4.1 operator-QA.py
@@ -0,0 +1,38 @@
+"""
+num1= int(input("Enter number 1 ="))
+num2= int(input("Enter number 2 ="))
+
+print(f"sum = {num1+num2}")
+print(f"Sub = {num1-num2}")
+print(f"mul ={num1*num2}")
+print(f"Remainder = {num1%num2}")
+"""
+
+"""
+#EVEN ODD
+
+numb=int(input("Enter number : "))
+
+if numb%2==0:
+    print("Even")
+else:
+    print("odd")
+
+"""
+"""
+# check voter age
+
+age=int(input("Enter your age :"))
+print(f"user can vote {age >=18}")
+print(f"User is senior citizen {age>=60}")
+
+"""
+
+sub1=int(input("Enter sub1 marks :"))
+sub2=int(input("Enter sub2 marks :"))
+sub3=int(input("Enter sub3 marks :"))
+
+total = sub1+sub2+sub3
+avg=total/3
+
+print(f"student total marks is {total} and Average is {avg:.2f} ")

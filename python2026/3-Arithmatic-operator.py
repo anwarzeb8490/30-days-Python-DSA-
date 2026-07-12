bf241b8c5cff5285d68667e8b613641d4d1f020a
anwarzeb8490 <anwarzeb8490@gmail.com>
2026-07-12 12:27:39 +0500

    program-Arithmetic op

---
+++ python2026/3-Arithmatic-operator.py
@@ -0,0 +1,28 @@
+a=20
+b=10
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+
+print(a//b)
+
+print(2**6)
+print(24%10)
+
+#operator precedence
+"""
+** exponent
+*,/,//,%  Multiplication , division
++,- addition subtraction
+
+"""
+print("operator precedence")
+print(2+3*4)
+print(10-2**3)
+print(10//2+2)
+print((2+3)*12)
+print(2-(23+3))
+
